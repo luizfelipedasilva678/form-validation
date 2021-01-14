8b98efc02d5dabaa0ae4c9a385af75cff2f727e7
Luiz <luizfelipedasilva678@gmail.com>
2021-01-14 20:51:46 -0300

    Starting FormValidator class

--- assets/js/class/FormValidator.js
+++ assets/js/class/FormValidator.js
@@ -1,3 +1,17 @@
 export default class FormValidator {
+    constructor(form) {
+        this.form = form;
+    }
 
+    emptyFields() {
+        for(let field of this.form) {
+            if(field.type === 'checkbox' || field.type === 'text' || field.type === 'password'){
+                if(field.value === '') {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
 }
---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+import cpfValidator from '../js/class/CpfValidator.js';
+import FormValidator from '../js/class/FormValidator.js';
+
+(function (){
+    let validateForm;
+
+    
+    document.addEventListener('submit', function(e){
+        e.preventDefault();
+        validateForm = new FormValidator(e.target);
+        if(validateForm.emptyFields()) {
+            console.log('Deu certo')
+        } else {
+            console.log('Falhou')
+        }
+        
+    })
+})();
